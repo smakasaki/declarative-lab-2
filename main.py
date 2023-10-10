7e5f018ee50151b4fb5d35d115f5db96fbba0726
Smelch228 <salamandr258@gmail.com>
2023-10-10 12:13:28 +0300

    added main parsing method

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import os
 import requests
+import time
+import json
+
+API_KEY = '9509ce90-8631-4769-b44e-ede406848cdb'
+HEADERS = {
+    'Content-Type': 'application/json',
+    'X-API-KEY': API_KEY,
+}
 
 def get_last_saved_file_number(path):
     try:
@@ -17,7 +25,43 @@ def save_review(review, review_type, file_number):
     file_path = f"{folder_path}/{file_name}.txt"
     
     os.makedirs(folder_path, exist_ok=True)
+    author_name = review['author'] if review['author'] is not None else "Anonymous"
     
     with open(file_path, 'w', encoding='utf-8') as file:
-        file.write(review['title'] + "\n")
+        file.write(author_name + "\n")
         file.write(review['description'])
+
+def get_reviews(movie_id, review_type, start_number):
+    page = 1
+    saved_reviews = start_number + 1
+    
+    while True:
+        url = f"https://kinopoiskapiunofficial.tech/api/v2.2/films/{movie_id}/reviews?page={page}&order=DATE_DESC"
+        response = requests.get(url, headers=HEADERS)
+        time.sleep(0.1)  # Соблюдение ограничения по скорости запросов
+        
+        if response.status_code != 200:
+            print(f"Failed to get data: {response.status_code}")
+            break
+        
+        data = response.json()
+        
+        for item in data['items']:
+            if item['type'] == review_type.upper():
+                save_review(item, review_type.lower(), saved_reviews)
+                saved_reviews += 1
+        
+        page += 1
+        if page > data['totalPages']:
+            break
+
+def main():
+    movie_ids = ['435', '195334', '535341']
+    for movie_id in movie_ids:
+        for review_type in ['positive', 'negative']:
+            folder_path = f"dataset/{review_type}"
+            last_saved_number = get_last_saved_file_number(folder_path)
+            get_reviews(movie_id, review_type, last_saved_number)
+
+if __name__ == "__main__":
+    main()
