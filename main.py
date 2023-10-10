ffd9ec94dc3c7d939d6ba4e751c0b57ff4b4b0ae
Smelch228 <salamandr258@gmail.com>
2023-10-10 12:01:01 +0300

    added function that gets last saved file's number

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import os
 import requests
 
+def get_last_saved_file_number(path):
+    try:
+        files = os.listdir(path)
+        if not files:
+            return -1
+        max_number = max(int(f.rstrip('.txt')) for f in files)
+        return max_number
+    except FileNotFoundError:
+        return -1
+
 def save_review(review, review_type, file_number):
     folder_path = f"dataset/{review_type}"
     file_name = str(file_number).zfill(4)  # Добавление ведущих нулей
